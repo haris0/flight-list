1d8dbd58de055492b26890b1474d1191fe3644ba
Haris <hari@pijar.com>
2025-12-17 14:33:16 +0700

    implement responsive layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,12 +11,23 @@ const FlightList = async () => {
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between p-5 bg-white dark:bg-black sm:items-start">
-        <Suspense fallback={<div>Loading flights...</div>}>
+    <div className="min-h-screen flex flex-col items-center">
+      <div className="w-full max-w-6xl flex flex-col flex-1">
+        <header
+          className="sticky top-0 md:top-3 p-4 h-28 md:h-fit shadow-md text-white md:m-3 md:rounded-lg text-sm"
+          style={{ background: '#00275a' }}
+        >
+          <div>
+            Jakarta (CGK) ⇒ Bali (DPS)
+          </div>
+          <div className="text-white text-xs">
+            22 Oktober 2025
+          </div>
+        </header>
+        <Suspense fallback={<div className="p-4">Loading flights...</div>}>
           <FlightList />
         </Suspense>
-      </main>
+      </div>
     </div>
   );
 }
--- src/pages/flight-list.tsx
+++ src/pages/flight-list.tsx
@@ -8,14 +8,22 @@ interface FlightListPageProps {
 
 const FlightListPage = ({ initialFlights }: FlightListPageProps) => {
   return (
-    <div className="w-full">
-      <ul>
-        {initialFlights.flights.map((flight) => (
-          <li key={flight.id}>
-            {flight.airline.name} - {flight.flightNumber}
-          </li>
-        ))}
-      </ul>
+    <div className="md:flex md:flex-1">
+      <aside className="hidden md:block w-82 p-4">
+        <div className="font-semibold sticky top-24">Sidebar Placeholder</div>
+      </aside>
+      <aside className="sticky md:hidden w-full p-4 top-18 shadow-xs -mt-10 rounded-t-2xl" style={{ background: '#f3f4f6' }}>
+        Filter Placeholder
+      </aside>
+      <main className="flex-1 p-4 md:mt-0">
+        <ul>
+          {initialFlights.flights.map((flight) => (
+            <li key={flight.id}>
+              {flight.airline.name} - {flight.flightNumber}
+            </li>
+          ))}
+        </ul>
+      </main>
     </div>
   );
 };
